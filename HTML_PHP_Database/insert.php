15cff1ecde7a69435c98879ec99bee81c7e95c0c
Ari Ramdhan <50137246+aricangcimen@users.noreply.github.com>
2019-05-29 20:34:27 +0700

    Update insert.php

--- HTML_PHP_Database/insert.php
+++ HTML_PHP_Database/insert.php
@@ -7,7 +7,20 @@ $phone = $_POST['phonecode'];
 
 if(!empty($username) || !empty($password)  ||  !empty($gender)  || !empty($email) || !empty ($phone) )
 {
+$host = "localhost";
+$dbUserName = "root";
+$dbpassword = "";
+$dbname = "youtube";
 
+$conn = new mysqli($host ,$dbUserName ,$dbpassword ,$dbname);
+if(mysqli_connect_error())
+{
+die('Connect Error ('. mysqli_connect_error())
+}else {
+
+    $SELECT = "SELECT  email From register Where  email = ? Limit 1";
+    $INSERT = "INSERT Into register (username , password , gender , email , phonecode ,phone ) values(?,?,?,?,?)";
+}
 
 }else {
 echo "All field are required";
