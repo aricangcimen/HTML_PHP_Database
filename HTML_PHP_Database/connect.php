6bc62e048c55337d5de1b094ac2ed23130fb9f69
aricangcimen <antosan1234567@gmail.com>
2019-05-29 10:20:55 +0700

    masukan input

--- HTML_PHP_Database/connect.php
+++ HTML_PHP_Database/connect.php
@@ -4,9 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Register Form</title>
 </head>
 <body>
-    
+<form>
+<table>
+    <tr>
+    <td> Name :     </td>
+    <td> <input type = "text" name = "username"></td>
+    </tr>
+    <tr>
+    <td>Password :   </td>
+    <td> <input type = "password" name = "password"></td>
+    </tr>
+    <tr>
+    <td> Gender : </td>
+     <input type = "radio" name = "gender" > Male
+     <input type = "radio" name = "gender" > Female
+    </td>
+    </tr>
+    <td> email: </td>
+    <td> <input type: "email"></td>
+    </tr>
+    <tr>
+    <td> Phone No : </td>
+    <td>
+    <select>
+
+
+
+
+
+</table>
+</form>  
 </body>
 </html>
