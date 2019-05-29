13d3402d05e7e49f5dddb01ca6db02dad30ebd9a
aricangcimen <antosan1234567@gmail.com>
2019-05-29 21:03:49 +0700

    lengkap

--- HTML_PHP_Database/insert.php
+++ HTML_PHP_Database/insert.php
@@ -19,9 +19,34 @@ die('Connect Error ('. mysqli_connect_error())
 }else {
 
     $SELECT = "SELECT  email From register Where  email = ? Limit 1";
-    $INSERT = "INSERT Into register (username , password , gender , email , phonecode ,phone ) values(?,?,?,?,?)";
+    $INSERT = "INSERT Into register (username , password , gender , email , phonecode ,phone ) values(?,?,?,?,?,?)";
+    //prepare statement
+    $stmt = $conn->prepare($SELECT);
+    $stmt -> bind_param("s" , $email);
+    $stmt -> execute();
+    $stmt -> bind_result($email);
+    $stmt -> store_result();
+    $rnum = $stmt -> num_rows;
+
+    if ($rnum==0){
+$stmt -> close();
+$stmt = $conn -> prepare($INSERT);
+$stmt -> bind_param("ssssii" , $username , $password , $gender , $email , $phoneCode ,$phone);
+$stmt -> execute();
+echo "New Record inserted sucesfully"
+
+
+    }else 
+    {
+        echo " some onev alredy registery with this email";
+
+    }
+    $stmt->close();
+    $conn->close();
+
+
 }
-.
+
 }else {
 echo "All field are required";
 die();
