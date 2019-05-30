7f49cba7730db1dace2b476f5bf4cbb6ce521c22
aricangcimen <antosan1234567@gmail.com>
2019-05-30 08:00:03 +0700

    login pro

--- HTML_PHP_Database/login_pro/connect1.php
+++ HTML_PHP_Database/login_pro/connect1.php
@@ -2,9 +2,39 @@
 $username = filter_input(INPUT_POST , 'username');
 $password = filter_input(INPUT_POST , 'password');
 
+if(!empty($username)){
+if(!empty($password)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "youtube";
 
+$conn = new mysqli ($host , $dbusername , $dbpassword , $dbname);
 
+if (mysqli_connect_error()){
+    die('Connect Error ('. mysqli_connect_errno().')'
+    .mysqli_connect_error());
+}else {
+$sql = "INSERT INTO account (username , password) values('$username , $password ')";
+if($conn->query($sql)){
+    echo "new record is inserted sucesfully";
 
+}else {
+echo "Error". $sql."<br>" . $conn->error;
+}
+$conn->close();
+
+}
+
+}else {
+echo "Username should not be empty ";
+die();
+}
+}else {
+echo "Username should not be empty ";
+die();
+
+}
 
 
 ?>
