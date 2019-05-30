74bcc16f1cfeee4ec676f4ce9b60fbbd1e527e59
aricangcimen <antosan1234567@gmail.com>
2019-05-30 10:45:20 +0700

    tampil2

--- HTML_PHP_Database/menampilkan_data/test.php
+++ HTML_PHP_Database/menampilkan_data/test.php
@@ -5,6 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Table With Database</title>
+    <style type = "text/css"></style>
+    table
+    {
+        border-collapse : collapse;
+        width : 100%;
+        color : #D9645;
+        font-family : monospace;
+        font-size : 20px;
+    }
+    th {
+        background-color : #f2f2f2;
+        color : white;
+    }
 
 </head>
 <body>
@@ -15,9 +28,25 @@
 <th>Password</th>
 </tr>
 <?php
-$conn = mysqli_connect
+$conn = mysqli_connect("localhost","root"," " , "company" );
+if($conn -> connect_error){
+die("Conecction  failed" . $conn -> connect_error);
+
+}
+$sql = "SELECT  id , username , password from login";
+$result = $conn -> query($sql);
+if($result -> num_rows > 0 ){
+    while($row = $result -> fetch_assoc()){
+echo "<tr><td>".$row["id"]."</td> <td>" .$row["username"]."</td> <td>".$row["password"]."</td></tr>;
+    }
+echo "</table>";
+}
+else {
+echo "0 result"
+$conn -> close();
 
 
+}
 
 ?>
     </table>
