8e390904db705cf109beea253aad5a6ca5f6a7bc
aricangcimen <antosan1234567@gmail.com>
2019-05-31 03:33:11 +0700

    masukan lagi

--- HTML_PHP_Database/bentuk_form/form1.php
+++ HTML_PHP_Database/bentuk_form/form1.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel = "stylesheet"href = "style1.css"
 </head>
 <body>
     <main>
@@ -13,7 +14,9 @@
     <input type = "text" name = "name" placeholder = "Full name">
     <input type = "text" name = "mail" placeholder = "YourEmail">
     <input type = "text" name = "subject" placeholder = "Subject">
-    <textarea name = "mame" rows = "cols= 80"></textarea>
+    <textarea name = "message" placeholder = "message"></textarea>
+    <button type = "submit" name = "submit">SEND E-MAIL</button>
+    </form>
     </main>
 </body>
 </html>
