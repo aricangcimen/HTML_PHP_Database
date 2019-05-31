9f19201310c522d57587dac62014d72a8e0f4c1d
aricangcimen <antosan1234567@gmail.com>
2019-05-31 18:48:26 +0700

    tambah form2 dan form3

---
+++ HTML_PHP_Database/bentuk_form/form3/formhanding.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method ="GET" action ="">
+    <input name = "text"  type = "text"/>
+    <input name = "text"  type = "password"/><
+    <input name = "submit"  type = "submit" value = "submit !!"/>
+    
+    
+    
+
+
+
+    </form>
+</body>
+</html>
