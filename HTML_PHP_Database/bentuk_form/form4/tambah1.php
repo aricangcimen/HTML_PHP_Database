0ccf02afb96cb38b9df3c5d237c808539fdcc188
aricangcimen <antosan1234567@gmail.com>
2019-06-01 06:45:30 +0700

    masih pusing

--- HTML_PHP_Database/bentuk_form/form4/tambah1.php
+++ HTML_PHP_Database/bentuk_form/form4/tambah1.php
@@ -3,18 +3,13 @@ if(isset ($_POST['tekan'])){
 $no1 = $_POST['nomor1'];
 $no2 = $_POST['nomor2'];
 $no3 = $_POST['nomor3'];
-echo $no1+$no2+$no3;
 
-
-
+echo "<br>" . $no1 . "</br>";
+echo "<br>" . $no2 . "</br>";
+echo "<br>" . $no3 . "</br>";
+$no4 = $no1 + $no2 + $no3;
+echo $no4 ;
 }
-
-
-
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,10 +20,12 @@ echo $no1+$no2+$no3;
 </head>
 <body>
 <form action ="" method = "POST" >
-    <input type = "text" name ="nomor1" value = "nilai satu "><br>
-    <input type = "text" name ="nomor2" value = "nilai dua" ><br>
-    <input type = "text" name = "nomor3" value = "nilai tiga"><br>
+    <input type = "text" name ="nomor1" ><br>
+    <input type = "text" name ="nomor2" ><br>
+    <input type = "text" name = "nomor3" ><br>
     <input type = "submit" name = "tekan" value = "pijit">
-
+   </form>
+  
+?>
 </body>
 </html>
