bc4dfb63a50f72fbb5c1838d6d66fea7d5902553
aricangcimen <antosan1234567@gmail.com>
2019-06-01 04:52:55 +0700

    input week

---
+++ HTML_PHP_Database/bentuk_form/form4/hari.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="POST">
+Select a week:<br>
+  <input type="week" name="week_year">
+<form>
+</body>
+</html>
